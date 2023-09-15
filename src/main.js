bfae4007ecfa3d61fd9f8bf17be264d54369aadc
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-09-15 14:48:20 +0100

    Use class instead of constructor function and made some minor changes

--- src/main.js
+++ src/main.js
@@ -1,24 +1,39 @@
-let myLibrary = [];
+let myLibrary = []
 
 // DOM elements
-const bookShelf = document.querySelector('.book-shelf');
-const modalOverlay = document.querySelector('.modal-overlay');
-const modalToggle = document.querySelector('.add-btn');
-const form = document.querySelector('form');
-const addNewBookBtn = document.querySelector('.add-new-book-btn');
+const bookShelf = document.querySelector('.book-shelf')
+const modalOverlay = document.querySelector('.modal-overlay')
+const modalToggle = document.querySelector('.add-btn')
+const form = document.querySelector('form')
+const addNewBookBtn = document.querySelector('.add-new-book-btn')
 
-function Book(title, author, pageCount, hasRead) {
-	this.title = title;
-	this.author = author;
-	this.pageCount = pageCount;
-	this.hasRead = hasRead;
-	this.toggleReadStatus = function () {
-		if (this.hasRead === 'on') {
-			this.hasRead = 'off';
-		} else {
-			this.hasRead = 'on';
-		}
-	};
+// function Book(title, author, pageCount, hasRead) {
+//   this.title = title
+//   this.author = author
+//   this.pageCount = pageCount
+//   this.hasRead = hasRead
+//   this.toggleReadStatus = function () {
+//     if (this.hasRead === 'on') {
+//       this.hasRead = 'off'
+//     } else {
+//       this.hasRead = 'on'
+//     }
+//   }
+// }
+class Book {
+  constructor(title, author, pageCount, hasRead) {
+    this.title = title
+    this.author = author
+    this.pageCount = pageCount
+    this.hasRead = hasRead
+  }
+  toggleReadStatus() {
+    if (this.hasRead === 'on') {
+      this.hasRead = 'off'
+    } else {
+      this.hasRead = 'on'
+    }
+  }
 }
 
 // Book.prototype.editBook = function () {
@@ -26,25 +41,25 @@ function Book(title, author, pageCount, hasRead) {
 // };
 
 function addBookToLibrary(title, author, pageCount, hasRead) {
-	let book = new Book(title, author, pageCount, hasRead);
-	myLibrary.push(book);
+  let book = new Book(title, author, pageCount, hasRead)
+  myLibrary.push(book)
 }
 
 function displayBooks() {
-	if (myLibrary.length === 0) {
-		bookShelf.className = 'book-shelf grid gap-2 pb-2';
-		bookShelf.classList.add('place-items-center');
-		bookShelf.innerHTML = `
+  if (myLibrary.length === 0) {
+    bookShelf.className = 'book-shelf grid gap-2 pb-2'
+    bookShelf.classList.add('place-items-center')
+    bookShelf.innerHTML = `
 			<h2>Book list is empty. Add new book</h2>
-		`;
-	} else {
-		bookShelf.className =
-			'book-shelf grid md:grid-cols-2 lg:grid-cols-4 gap-2 pb-2';
-		bookShelf.classList.remove('place-items-center');
-		myLibrary.map((book, index) => {
-			let bookCard = document.createElement('div');
-			bookCard.className = 'card bg-mint-5 rounded-lg shadow-md md:col-6 p-4';
-			bookCard.innerHTML = `
+		`
+  } else {
+    bookShelf.className =
+      'book-shelf grid md:grid-cols-2 lg:grid-cols-4 gap-2 pb-2'
+    bookShelf.classList.remove('place-items-center')
+    myLibrary.map((book, index) => {
+      let bookCard = document.createElement('div')
+      bookCard.className = 'card bg-mint-5 rounded-lg shadow-md md:col-6 p-4'
+      bookCard.innerHTML = `
 				<h2 class="font-bold text-xl">${book.title}</h2>
 				<h3 class="font-bold">${book.author}</h3>
 				<h3>${book.pageCount} Pages</h3>
@@ -57,58 +72,58 @@ function displayBooks() {
 						<i class='bx bxs-trash'></i>
 					</button>
 				</div>
-			`;
-			bookShelf.appendChild(bookCard);
-		});
-		let editbtns = document.querySelectorAll('.edit-btn');
-		let deleteBtns = document.querySelectorAll('.delete-btn');
-		editbtns.forEach((element) => {
-			element.onclick = (e) => {
-				console.log(myLibrary[element.dataset.bookId]);
-				myLibrary[element.dataset.bookId].toggleReadStatus();
-				bookShelf.innerHTML = '';
-				displayBooks();
-			};
-		});
-		deleteBtns.forEach((element) => {
-			element.onclick = (e) => {
-				bookShelf.innerHTML = '';
-				deleteBook(element.dataset.bookId);
-			};
-			// deleteBook(element.dataset.bookId);
-		});
-	}
+			`
+      bookShelf.appendChild(bookCard)
+    })
+    let editbtns = document.querySelectorAll('.edit-btn')
+    let deleteBtns = document.querySelectorAll('.delete-btn')
+    editbtns.forEach((element) => {
+      element.onclick = (e) => {
+        console.log(myLibrary[element.dataset.bookId])
+        myLibrary[element.dataset.bookId].toggleReadStatus()
+        bookShelf.innerHTML = ''
+        displayBooks()
+      }
+    })
+    deleteBtns.forEach((element) => {
+      element.onclick = (e) => {
+        bookShelf.innerHTML = ''
+        deleteBook(element.dataset.bookId)
+      }
+      // deleteBook(element.dataset.bookId);
+    })
+  }
 }
 
-displayBooks();
+displayBooks()
 
 function displayForm() {
-	modalToggle.classList.toggle('rotate');
-	modalOverlay.classList.toggle('hidden');
-	modalOverlay.classList.toggle('fade');
-	modalOverlay.classList.toggle('grid');
+  modalToggle.classList.toggle('rotate')
+  modalOverlay.classList.toggle('hidden')
+  modalOverlay.classList.toggle('fade')
+  modalOverlay.classList.toggle('grid')
 }
 
-modalToggle.onclick = displayForm;
+modalToggle.onclick = displayForm
 
 form.onsubmit = (e) => {
-	e.preventDefault();
-	const data = new FormData(form);
-	const json = Object.fromEntries(data.entries());
-	console.log(json);
-	addBookToLibrary(
-		json['book-title'],
-		json['book-author'],
-		json.pages,
-		json.complete
-	);
-	form.reset();
-	bookShelf.innerHTML = '';
-	displayBooks();
+  e.preventDefault()
+  const data = new FormData(form)
+  const json = Object.fromEntries(data.entries())
+  console.log(json)
+  addBookToLibrary(
+    json['book-title'],
+    json['book-author'],
+    json.pages,
+    json.complete
+  )
+  form.reset()
+  bookShelf.innerHTML = ''
+  displayBooks()
 
-	console.log(myLibrary);
-};
+  console.log(myLibrary)
+}
 function deleteBook(bookIndex) {
-	myLibrary = myLibrary.filter((item, index) => index != bookIndex);
-	displayBooks();
+  myLibrary = myLibrary.filter((item, index) => index != bookIndex)
+  displayBooks()
 }
